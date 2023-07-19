20122e26ce01b7c0de72d06450cd9a9b0383edf8
Ryan Camp <ryancamp@singleops.com>
2023-07-19 12:55:18 -0400

    Setup model relationships

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ApplicationRecord
+  has_many :plant_vendors
+  has_many :vendors, through: :plant_vendors
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,4 @@
 class Vendor < ApplicationRecord
+  has_many :plant_vendors
+  has_many :plants, through: :plant_vendors
 end
